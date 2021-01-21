afb33c844723e14eaf4785db72521e8dbf2dffcb
thayron <thayronf3@gmail.com>
2021-01-21 00:37:46 -0300

    FOi criado a estilização do deshBorad

--- src/pages/Deshboard/index.tsx
+++ src/pages/Deshboard/index.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronsRight } from 'react-icons/fi';
+
+import { Title, Form, Repositories } from './styles';
+import logo from '../../image/logo.svg';
 
 const Dahboard: React.FC = () => (
-  <Title>
-    Explore repositórios no GitHub
-  </Title>
+  <>
+    <img src={logo} alt="" />
+    <Title>
+      Explore repositórios no GitHub
+    </Title>
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">pesquisar</button>
+    </Form>
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars2.githubusercontent.com/u/42882103?s=460&u=719625a4340f30ad2790d4a0714811a629d1b0d2&v=4"
+          alt="thayron feitosa"
+        />
+        <div>
+          <strong>repositório qualquer</strong>
+          <p>Descrição do repositório</p>
+        </div>
+        <FiChevronsRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars2.githubusercontent.com/u/42882103?s=460&u=719625a4340f30ad2790d4a0714811a629d1b0d2&v=4"
+          alt="thayron feitosa"
+        />
+        <div>
+          <strong>repositório qualquer</strong>
+          <p>Descrição do repositório</p>
+        </div>
+        <FiChevronsRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars2.githubusercontent.com/u/42882103?s=460&u=719625a4340f30ad2790d4a0714811a629d1b0d2&v=4"
+          alt="thayron feitosa"
+        />
+        <div>
+          <strong>repositório qualquer</strong>
+          <p>Descrição do repositório</p>
+        </div>
+        <FiChevronsRight size={20} />
+      </a>
+    </Repositories>
+  </>
 );
 
 export default Dahboard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,11 @@ import { Switch, Route } from 'react-router-dom';
 import Dahboard from '../pages/Deshboard';
 import Repository from '../pages/Repository';
 import NotFound from '../pages/NotFound';
+
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dahboard} />
+    <Route path="repository" exact component={Repository} />
     <Route path="*" exact component={NotFound} />
   </Switch>
 );
