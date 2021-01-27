7f9ee6f21108ba81ee160d505fbc2968e4a5c155
thayron <thayronf3@gmail.com>
2021-01-27 05:33:56 -0300

    Foi estilizado a pagina de repositories

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,16 +1,60 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronsRight } from 'react-icons/fi';
+import logo from '../../image/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string
 }
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
   return (
-    <h1>
-      Repository:
-      {params.repository}
-    </h1>
+    <>
+      <Header>
+        <img src={logo} alt="imager git " />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src={logo} alt="" />
+          <div>
+            <strong>Roasdfasdf</strong>
+            <p>asdfasdfasdf</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1080</strong>
+            <span>Starts</span>
+          </li>
+          <li>
+            <strong>1080</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1080</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="sdfasdf">
+          <div>
+            <strong>adsf</strong>
+            <p>asdf</p>
+          </div>
+          <FiChevronsRight size={20} />
+        </Link>
+      </Issues>
+
+    </>
   );
 };
 
